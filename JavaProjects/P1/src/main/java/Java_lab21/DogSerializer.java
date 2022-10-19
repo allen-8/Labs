a53cb8914eda08e83c87a325250e232263476d82
Allen <mlegenchyk@gmail.com>
2022-10-19 17:00:46 +0200

    Added new labs

---
+++ JavaProjects/P1/src/main/java/Java_lab21/DogSerializer.java
@@ -0,0 +1,48 @@
+package Java_lab21;
+
+import java.io.*;
+import com.google.gson.*;
+
+public class DogSerializer {
+    public static void main(String[] args) throws IOException {
+        Dog sharik = new Dog("Sharik", 3);
+        serialize(sharik);
+        Dog dog = deserialize();
+        System.out.println(dog.name + " " + dog.age);
+    }
+    public static void serialize(Dog dog) throws IOException {
+        Gson gson = new GsonBuilder().create();
+        try {
+            FileWriter f = new FileWriter("dog.json");
+            f.write(gson.toJson(dog));
+            f.close();
+        }
+        catch (Exception e)
+        {
+            System.err.println("Cannot open the file dog.json!");
+        }
+    }
+    public static Dog deserialize() throws IOException
+    {
+        Gson gson = new GsonBuilder().create();
+        try {
+            FileReader f = new FileReader("dog.json");
+            return gson.fromJson(f, Dog.class);
+        }
+        catch (Exception e) {
+            System.err.println("Cannot open the file dog.json!");
+        }
+        return null;
+    }
+}
+
+class Dog implements Serializable {
+    public String name;
+    public int age;
+
+    public Dog(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+}
